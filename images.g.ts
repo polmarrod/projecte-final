f4184745014e24f836bdb0ad29f8c29b2ff6d98e
Margaux Boileau <144930772+Margaux-Boileau@users.noreply.github.com>
2023-12-10 11:54:30 +0100

    prizes-assets

---
+++ images.g.ts
@@ -0,0 +1,102 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . 
+4 d d d d d d d d d d d d d d f 
+4 d f d d d d d d d d d d f d f 
+4 d d d d 4 4 4 4 4 d d d d d f 
+4 d d d 4 4 f f f 4 4 d d d d f 
+4 d d d 4 4 f d d 4 4 f d d d f 
+4 d d d 4 4 f d d 4 4 f d d d f 
+4 d d d d f f d 4 4 4 f d d d f 
+4 d d d d d d 4 4 f f f d d d f 
+4 d d d d d d 4 4 f d d d d d f 
+4 d d d d d d d f f d d d d d f 
+4 d d d d d d 4 4 d d d d d d f 
+4 d d d d d d 4 4 f d d d d d f 
+4 d f d d d d d f f d d d f d f 
+4 d d d d d d d d d d d d d d f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "image3":
+            case "shroom_sprite1":return img`
+. . . . . . e e e e . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e e e e e e e . . . . 
+. . . e e e e e e e e e e . . . 
+. . e f f e e e e e e f f e . . 
+. e e e d f e e e e f d e e e . 
+. e e e d f f f f f f d e e e . 
+e e e e d f d e e d f d e e e e 
+e e e e d d d e e d d d e e e e 
+e e e e e e e e e e e e e e e e 
+. e e e e d d d d d d e e e e . 
+. . . . d d d d d d d d . . . . 
+. . f f d d d d d d d d . . . . 
+. f f f f d d d d d d f f . . . 
+. f f f f f d d d d f f f . . . 
+. . f f f f f . . f f f . . . . 
+`;
+            case "image4":
+            case "shroom_sprite2":return img`
+. . . . . . e e e e . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e e e e e e e . . . . 
+. . . e e e e e e e e e e . . . 
+. . e f f e e e e e e f f e . . 
+. e e e d f e e e e f d e e e . 
+. e e e d f f f f f f d e e e . 
+e e e e d f d e e d f d e e e e 
+e e e e d d d e e d d d e e e e 
+e e e e e e e e e e e e e e e e 
+. e e e e d d d d d d e e e e . 
+. . . . d d d d d d d d . . . . 
+. . . . d d d d d d d d f f . . 
+. . . f f d d d d d d f f f f . 
+. . . f f f d d d d f f f f f . 
+. . . . f f f . . f f f f f . . 
+`;
+            case "image2":
+            case "shroom_sprite0":return img`
+. . . . . . e e e e . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e e e e e e e . . . . 
+. . . e e e e e e e e e e . . . 
+. . e f f e e e e e e f f e . . 
+. e e e d f e e e e f d e e e . 
+. e e e d f f f f f f d e e e . 
+e e e e d f d e e d f d e e e e 
+e e e e d d d e e d d d e e e e 
+e e e e e e e e e e e e e e e e 
+. e e e e d d d d d d e e e e . 
+. . . . d d d d d d d d . . . . 
+. . . . d d d d d d d d . . . . 
+. . . f f d d d d d d f f . . . 
+. . . f f f d d d d f f f . . . 
+. . . . f f f . . f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
